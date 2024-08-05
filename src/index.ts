867a69488c7abd1e18b28787fd9d3ff7acf80382
YubaC <49724342+YubaC@users.noreply.github.com>
2024-08-05 09:14:10 +0800

    Refactor: Rename `_inputBar.scss` to `_input-bar.scss`

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,5 @@ import "./data/ai/gemini";
 import "./data/panel/inputBar";
 import "./render/utility";
 import "@assets/scss/index.scss";
-import "@assets/scss/_inputBar.scss";
 
 keyboardManager.start();
